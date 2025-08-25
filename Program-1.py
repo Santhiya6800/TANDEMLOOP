df2a9ff327b406a09d5491db4f92a340eead1070
Santhiya6800 <126955244+Santhiya6800@users.noreply.github.com>
2025-08-25 15:18:39 +0530

    Create Program-1.py

---
+++ Program-1.py
@@ -0,0 +1,27 @@
+class Calculator:
+    def __init__(self, a, b, operation):
+        self.a = a
+        self.b = b
+        self.operation = operation.lower()
+
+    def calculate(self):
+        if self.operation == "add":
+            return self.a + self.b
+        elif self.operation == "sub":
+            return self.a - self.b
+        elif self.operation == "mul":
+            return self.a * self.b
+        elif self.operation == "div":
+            if self.b != 0:
+                return self.a / self.b
+            else:
+                return "Error: Division by zero"
+        else:
+            return "Invalid Operation"
+
+if __name__ == "__main__":
+    a = float(input())
+    b = float(input())
+    op = input()
+    calc = Calculator(a, b, op)
+    print(calc.calculate())
